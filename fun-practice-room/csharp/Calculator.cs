6a5fd31079413f6b4894523cde36a72ee70bf93f
HappyRetriver <choijonjonho@gmail.com>
2025-12-13 12:34:37 +0900

    계산기
    사칙연산 계산기를 만들어보면서 if의 조건 문과 while의 반복과 break를 이해하는 시간을 가졌습니다

---
+++ fun-practice-room/csharp/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace choijaewon{
+    class progam{
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("숫자를 입력하시오");
+            int x = int.Parse(Console.ReadLine());
+
+            char z;
+            while (true)
+            {
+                Console.WriteLine("+, -, /, *");
+                z = Console.ReadLine()[0];
+
+                if (z == '+' || z == '-' || z == '/' || z == '*')
+                    break;
+            }
+            Console.WriteLine("숫자를 입력하시오");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("입력된 값");
+            Console.WriteLine($"{x}{z}{y}");
+
+            int result = 0;
+            if (z == '+')
+            { result = x + y; }
+            else if (z == '-') { result = x - y; }
+            else if (z == '*') { result = x * y; }
+            else if (z == '/') { result = x / y; }
+
+            Console.WriteLine("값은");
+            Console.WriteLine($"{result}");
+
+            Console.WriteLine("초기화p, 다시시작 엔터");
+            string input = Console.ReadLine();
+                if (input =="q") ;
+                break;
+            }
+        }
+    }
+   
+}
