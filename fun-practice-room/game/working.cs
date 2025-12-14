fb24fba7d3777497204e75f3253b262c5542f5df
HappyRetriver <choijonjonho@gmail.com>
2025-12-14 18:26:01 +0900

    Create working.cs

---
+++ fun-practice-room/game/working.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class working : MonoBehaviour
+{
+    public float speed = 5f;      
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");  
+        float y = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(x, y).normalized;
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * speed;
+    }
+}
