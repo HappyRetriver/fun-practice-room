32776c7deae98fd5e837d9ff466c1eb314f2caff
HappyRetriver <choijonjonho@gmail.com>
2025-12-18 22:34:44 +0900

    Create simple0.cs

---
+++ fun-practice-room/csharp/simple0.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace choijaewon
+{
+    public class ch1 
+    { 
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("값을 입력하시오");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                    number += 2;
+                else
+                    number += 1;
+                Console.Write("결과 값:");
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
