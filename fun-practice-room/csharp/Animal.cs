6d2202fdbff338cdb3c6d723c8f715c4a21c0db7
HappyRetriver <choijonjonho@gmail.com>
2025-12-19 19:37:06 +0900

    Animal

---
+++ fun-practice-room/csharp/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace choi {
+    abstract class Animal 
+    {
+        public abstract void speak();
+    }
+
+    class dog : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("강아지");
+        }
+    }
+    class deer : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("사슴");
+        }
+    }
+    class cat : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("고양이");
+        }
+
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1:강아지, 2: 고양이, 3:사슴 ");
+            int input = int.Parse(Console.ReadLine());
+            Animal animal = null;
+            switch (input)
+            {
+                case 1:
+                    animal = new dog();
+                    break;
+                case 2:
+                    animal = new cat();
+                    break;
+                case 3:
+                    animal = new deer();
+                    break;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    return;
+            }
+            animal.speak();
+        }
+    }
+}
---
+++ fun-practice-room/csharp/Inheritance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace choi 
+{
+    public class perent{
+        public virtual void speak()
+        {
+            Console.WriteLine("부모동작");
+        }
+    }
+    class chid : perent {
+        public override void speak()
+        {
+            Console.WriteLine("부모동작");
+        }
+       
+    }
+
+}
